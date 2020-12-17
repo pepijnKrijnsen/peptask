91014c3accc8812c2bb026e8cdc578503359cac2
Pepijn K <pepijn@krijnsen.com>
2020-12-17 21:16:37 +0000

    New task logic works

--- peptask/model.py
+++ peptask/model.py
@@ -1,6 +1,6 @@
 def newTask(title, due_date):
     # data validation
-    if not isalnum(title):
+    if not title.isalnum():
         error = "Title must be alpha-numeric."
         return error
     from datetime import datetime as dt
@@ -8,16 +8,20 @@ def newTask(title, due_date):
     if due_date < fmnow:
         error = "Due date cannot be in the past."
         return error
+
     # get the latest unique id
     l = open("peptask/tasks/_latest")
-    last_unique = l.read(); l.close()
+    last_unique = int(l.read()); l.close()
     last_unique += 1
+    last_unique = str(last_unique)
     l = open("peptask/tasks/_latest", "w")
     l.write(last_unique); l.close()
+
     # writing the file
-    new_task_file = '{ "title": {}, "due_by": {} }'.format(title, due_date)
+    new_task_file = '{ "title": "%s", "due_by": "%s" }' % (title, due_date)
     nt = open("peptask/tasks/active/{}".format(last_unique), "x")
     nt.write(new_task_file); nt.close()
+    print(new_task_file)
 
 
 def loadTasks():
--- peptask/view.py
+++ peptask/view.py
@@ -1,24 +1,28 @@
 import functools
-from flask import url_for, render_template, Blueprint
+from flask import url_for, render_template, Blueprint, request
 
 from . import model
 
 
 bp = Blueprint("view", __name__)
 
-@bp.route("/", methods=("GET", "POST"))
+@bp.route("/", methods = ("GET", "POST"))
 def index():
-    if method == "POST":
+    if request.method == "POST":
+        title = request.form["title"]
+        due_date = request.form["duedate"]
         model.newTask(title, due_date)
     tasks = model.loadTasks()
     return render_template("index.html", tasks = tasks)
 
 
-@bp.route("/new", methods=("GET", "POST"))
-def newTask():
-    pass
-
-
-@bp.route("/complete", methods=("GET", "POST"))
-def completeTask():
-    pass
+# @bp.route("/new", methods = ["POST"])
+# def newTask():
+#     title = request.form["title"]
+#     due_date = request.form["due_date"]
+#     model.newTask(title, due_date)
+# 
+# 
+# @bp.route("/complete", methods=("GET", "POST"))
+# def completeTask():
+#     pass
